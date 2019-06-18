323dd5daa5eb41dd077754edbad72e730efe63e5
roy renno <royrenno@yahoo.com>
2019-06-18 00:19:21 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,5 +10,7 @@ def create_project_hash
 
   kickstarter.css("li.projects.grid_4").each do|projects|
     title: project.css ("h2.bbcard_name strong a").text
+    image link: project.css("div.project-thumbnail a img").attribute("src").value
+    description: project.css("p.bbcard_blub").text
 end
 end
