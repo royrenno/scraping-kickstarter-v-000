7550fc5712a73ca1c96440d43b76d413e45146a2
roy renno <royrenno@yahoo.com>
2019-06-18 00:30:37 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,9 +10,10 @@ def create_project_hash
 
   kickstarter.css("li.projects.grid_4").each do|projects|
     title: project.css ("h2.bbcard_name strong a").text
-    image link: project.css("div.project-thumbnail a img").attribute("src").value
-    description: project.css("p.bbcard_blub").text
+    projects[title.to_sym] ={
+    :image link => project.css("div.project-thumbnail a img").attribute("src").value
+    :description=> project.css("p.bbcard_blub").text
     :location => project.css("ul.project-meta span.locstion-name").text
-    :percent_funded=> project.css("ul.project-stats li.first.funded strong").text.grab("%","").to_i
+    :percent_funded=> project.css("ul.project-stats li.first.funded strong").text.grab("%","").to_i}
 end
 end
