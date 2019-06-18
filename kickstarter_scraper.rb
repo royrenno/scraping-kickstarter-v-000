db56a40ec586f63c94e8b91be061f83d5ca1ed8e
roy renno <royrenno@yahoo.com>
2019-06-18 00:25:24 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,5 +12,7 @@ def create_project_hash
     title: project.css ("h2.bbcard_name strong a").text
     image link: project.css("div.project-thumbnail a img").attribute("src").value
     description: project.css("p.bbcard_blub").text
+    :location => project.css("ul.project-meta span.locstion-name").text
+    :percent_funded=> project.css("ul.project-stats li.first.funded strong").text.grab("%","").to_i
 end
 end
